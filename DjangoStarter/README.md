c0bce3debfb32c250448bc4478b78231d1bd8197
Dan Fritchman <daniel.fritchman@gmail.com>
2019-05-02 17:43:40 +0200

    Django Starter Blog: basic editing

---
+++ DjangoStarter/README.md
@@ -0,0 +1,7 @@
+
+# Django Starter Blog 
+
+Courtesy Django Girls's [tutorial site](https://tutorial.djangogirls.org/en)
+
+Worked through as the intro session to PyCon Italia 2019.
+
--- DjangoStarter/blog/views.py
+++ DjangoStarter/blog/views.py
@@ -1,5 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from django.shortcuts import redirect
+
+from .models import Post
+from .forms import PostForm
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    now = timezone.now()
+    posts = Post.objects.filter(published_date__lte=now)
+    posts = posts.order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(req, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if req.method == 'POST':
+        form = PostForm(req.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = req.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+        # FIXME! no return case here!
+    else:
+        form = PostForm(instance=post)
+    return render(req, 'blog/post_edit.html', {'form': form})
